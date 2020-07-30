5c8bbb602691f7749773a6cdb995b88002e502b4
Sai Charan Uppari <55044602+SaiCharanUppari@users.noreply.github.com>
2020-07-30 08:37:20 +0530

    Homepage created

---
+++ src/main/java/com/charan/homepageappspringbootwar/HomepageAppSpringbootWarApplication.java
@@ -0,0 +1,13 @@
+package com.charan.homepageappspringbootwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageAppSpringbootWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageAppSpringbootWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charan/homepageappspringbootwar/HomepageAppSpringbootWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charan.homepageappspringbootwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageAppSpringbootWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
